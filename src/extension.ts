ff6a5a4e1a542ccadaffac84c75aeea71a874887
Pilmo <pilmokim99@gmail.com>
2024-01-24 12:43:29 +0900

    Add hooking feature (file events)

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,20 @@ import { commands, ExtensionContext } from "vscode";
 import { ComponentGalleryPanel } from "./panels/ComponentGalleryPanel";
 import * as vscode from "vscode";
 import { VMSideBar } from "./sidebar";
+import * as path from "path";
 
 export function activate(context: ExtensionContext) {
+  vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
+    if (editor) {
+      const filePath = editor.document.fileName;
+
+      if (path.basename(filePath) === "config.yml") {
+        const fileContent = editor.document.getText();
+        console.log("config content: ", fileContent);
+      }
+    }
+  });
+
   // Create the show gallery command
   const showGalleryCommand = commands.registerCommand("perseous.showUI", () => {
     ComponentGalleryPanel.render(context.extensionUri);
