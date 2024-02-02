d17656396a579b88ff5a5e6d0fbeed6aca0e20fb
Pilmo <pilmokim99@gmail.com>
2024-02-02 17:57:12 +0900

    get yaml data from user's config

--- src/extension.ts
+++ src/extension.ts
@@ -21,8 +21,11 @@ export function activate(context: ExtensionContext) {
       if (path.basename(filePath) === "config.yml") {
         const fileContent = editor.document.getText();
         const parsedYaml = yaml.parse(fileContent);
-        ComponentGalleryPanel.render(context.extensionUri);
+        const yamlData = JSON.stringify(parsedYaml);
+        // TODO: You should send parsed Data using parsedYaml not using local config.yml
+        // because the content of config.yml can be different from user.
 
+        ComponentGalleryPanel.render(context.extensionUri, yamlData);
         const a = createVmStatusBarItem(parsedYaml.vm0, "myExtension.vm0");
         const b = createVmStatusBarItem(parsedYaml.vm1, "myExtension.vm1");
         context.subscriptions.push(a, b);
@@ -30,15 +33,11 @@ export function activate(context: ExtensionContext) {
     }
   });
   // Create the show gallery command
-  const showGalleryCommand = commands.registerCommand("perseous.showUI", () => {
-    // const yamlString = fs.readFileSync(context.extensionUri.path + "/config.yml", "utf8");
-    // console.log();
-    // const data = yaml.parse(yamlString);
-    // console.log("Parsed YAML:", data.soc);
-    ComponentGalleryPanel.render(context.extensionUri);
-  });
-  // Add command to the extension context
-  context.subscriptions.push(showGalleryCommand);
+  // const showGalleryCommand = commands.registerCommand("perseous.showUI", () => {
+  //   ComponentGalleryPanel.render(context.extensionUri);
+  // });
+  // // Add command to the extension context
+  // context.subscriptions.push(showGalleryCommand);
 }
 
 function createVmStatusBarItem(vmConfig: VmConfig, command: string) {
