dbe7c4b8dd82f4279cb04fab15f67457194b53da
Pilmo <pilmokim99@gmail.com>
2024-02-06 17:38:03 +0900

    add option Change feature

--- webview-ui/src/Host.tsx
+++ webview-ui/src/Host.tsx
@@ -1,6 +1,19 @@
 import { VSCodeDropdown, VSCodeOption, VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import { useState } from "react";
+
+const vendorOption = ["ST", "NXP"];
 
 export function Host() {
+  const [deviceOptions, setDeviceOptions] = useState(["SR6X7"]);
+  const handleVendorChange = (event: any) => {
+    const selectedValue = event.target.value;
+    if (selectedValue === "ST") {
+      setDeviceOptions(["SR6x7"]);
+    } else if (selectedValue === "NXP") {
+      // Handle NXP vendor if needed
+      setDeviceOptions(["S32E270", "S32E275"]);
+    }
+  };
   return (
     <section className="component-container">
       <h2>Host</h2>
@@ -9,18 +22,18 @@ export function Host() {
           <div className="wrap-container">
             <div className="row-container">
               <p>Vendor</p>
-              <VSCodeDropdown position="below">
-                <VSCodeOption>ST</VSCodeOption>
-                <VSCodeOption>Option Label #2</VSCodeOption>
-                <VSCodeOption>Option Label #3</VSCodeOption>
+              <VSCodeDropdown onChange={handleVendorChange} position="below">
+                {vendorOption.map((option, index) => (
+                  <VSCodeOption key={index}>{option}</VSCodeOption>
+                ))}
               </VSCodeDropdown>
             </div>
             <div className="row-container">
               <p>Device</p>
               <VSCodeDropdown position="below">
-                <VSCodeOption>SR6X7</VSCodeOption>
-                <VSCodeOption>Option Label #2</VSCodeOption>
-                <VSCodeOption>Option Label #3</VSCodeOption>
+                {deviceOptions.map((option, index) => (
+                  <VSCodeOption key={index}>{option}</VSCodeOption>
+                ))}
               </VSCodeDropdown>
             </div>
           </div>
