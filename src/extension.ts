de6fdbfd71ae7a238a65a55e4d64d7d40d058737
Pilmo <pilmokim99@gmail.com>
2024-02-14 16:44:41 +0900

    annotate unused things

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,24 @@
 import { commands, ExtensionContext } from "vscode";
 import { ComponentGalleryPanel } from "./panels/ComponentGalleryPanel";
 import * as vscode from "vscode";
-import { VMSideBar } from "./sidebar";
+// import { VMSideBar } from "./sidebar";
 import * as path from "path";
 import * as yaml from "yaml";
 
-interface VmConfig {
-  name: string;
-}
+// interface VmConfig {
+//   name: string;
+// }
 
 export function activate(context: ExtensionContext) {
-  let currentPanel: vscode.WebviewPanel | undefined = undefined;
-
   vscode.workspace.onDidSaveTextDocument((document) => {
     if (document.fileName.endsWith("config.yml")) {
       const fileContent = document.getText();
       const parsedYaml = yaml.parse(fileContent);
       const yamlData = JSON.stringify(parsedYaml);
       ComponentGalleryPanel.render(context.extensionUri, yamlData);
-      const a = createVmStatusBarItem(parsedYaml.vm0, "myExtension.vm0");
-      const b = createVmStatusBarItem(parsedYaml.vm1, "myExtension.vm1");
-      context.subscriptions.push(a, b);
+      // const a = createVmStatusBarItem(parsedYaml.vm0, "myExtension.vm0");
+      // const b = createVmStatusBarItem(parsedYaml.vm1, "myExtension.vm1");
+      // context.subscriptions.push(a, b);
     }
   });
   vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
@@ -31,33 +29,33 @@ export function activate(context: ExtensionContext) {
         const parsedYaml = yaml.parse(fileContent);
         const yamlData = JSON.stringify(parsedYaml);
         ComponentGalleryPanel.render(context.extensionUri, yamlData);
-        const a = createVmStatusBarItem(parsedYaml.vm0, "myExtension.vm0");
-        const b = createVmStatusBarItem(parsedYaml.vm1, "myExtension.vm1");
-        context.subscriptions.push(a, b);
+        // const a = createVmStatusBarItem(parsedYaml.vm0, "myExtension.vm0");
+        // const b = createVmStatusBarItem(parsedYaml.vm1, "myExtension.vm1");
+        // context.subscriptions.push(a, b);
       }
     }
   });
-  const showGalleryCommand = commands.registerCommand("perseous.showUI", () => {
-    // ComponentGalleryPanel.render(context.extensionUri);
-    vscode.window.showInformationMessage("Button clicked!");
-  });
+  // const showGalleryCommand = commands.registerCommand("perseous.showUI", () => {
+  //   // ComponentGalleryPanel.render(context.extensionUri);
+  //   vscode.window.showInformationMessage("Button clicked!");
+  // });
   // Add command to the extension context
   // context.subscriptions.push(showGalleryCommand);
 }
 
-function createVmStatusBarItem(vmConfig: VmConfig, command: string) {
-  const disposable = vscode.commands.registerCommand(command, () => {
-    vscode.window.showInformationMessage("Button clicked!");
-    const treeDataProvider = new VMSideBar(vmConfig);
-    const treeView = vscode.window.createTreeView("memory.treeView", {
-      treeDataProvider,
-    });
-  });
+// function createVmStatusBarItem(vmConfig: VmConfig, command: string) {
+//   const disposable = vscode.commands.registerCommand(command, () => {
+//     vscode.window.showInformationMessage("Button clicked!");
+//     const treeDataProvider = new VMSideBar(vmConfig);
+//     const treeView = vscode.window.createTreeView("memory.treeView", {
+//       treeDataProvider,
+//     });
+//   });
 
-  const sidebarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-  sidebarItem.text = `$(chip) ${vmConfig.name}`;
-  sidebarItem.command = command;
-  sidebarItem.show();
+//   const sidebarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+//   sidebarItem.text = `$(chip) ${vmConfig.name}`;
+//   sidebarItem.command = command;
+//   sidebarItem.show();
 
-  return sidebarItem;
-}
+//   return sidebarItem;
+// }
